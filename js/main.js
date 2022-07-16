eddf6eb1985ee391337332f30cd5b2edc5650603
Toni <antonioraweb@gmail.com>
2022-07-16 15:28:47 -0500

    the magic ball

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const ball = document.getElementsByClassName("ball");
+const message = document.getElementById("respondMessage");
+
+// function mouseover and mouseout in ball
+const moveBall = (ballMagic) => {
+	ballMagic[0].addEventListener('mouseover', () => {
+		ballMagic[0].classList.toggle("ball__Animation");
+	})
+}
+
+const noMoveBall = (ballMagic) => {
+	ballMagic[0].addEventListener('mouseout', () => {
+		ballMagic[0].classList.remove("ball__Animation");
+		ballMagic[0].classList.toggle("ball__nomove");
+	})
+}
+
+// this function is the logical of the ball
+const magicBall = (ballMagic, ballMessage) => {
+	ballMagic[0].addEventListener("click", () => {
+		ballMagic[0].classList.toggle("respond")
+		const numberMagic = Math.floor(Math.random() * 10)
+		ballMagic[0].addEventListener("click", () => {
+			ballMessage.classList.toggle("hide")
+		})
+		if (numberMagic >= 0 && numberMagic < 2) {
+			ballMessage.innerHTML = `NO`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		} else if (numberMagic >= 2 && numberMagic < 4) {
+			ballMessage.innerHTML = `It's not possible`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		} else if (numberMagic >= 4 && numberMagic < 6) {
+			ballMessage.innerHTML = `Definitely not`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		} else if (numberMagic >= 6 && numberMagic < 8) {
+			ballMessage.innerHTML = `It's possible`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		} else if (numberMagic >= 8 && numberMagic < 10) {
+			ballMessage.innerHTML = `Yes`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		} else {
+			ballMessage.innerHTML = `I don't understand`
+			ballMessage.classList.remove("hide")
+			ballMessage.classList.add("message__ball")
+		}
+	})
+
+}
+
+moveBall(ball);
+noMoveBall(ball);
+magicBall(ball, message);
---
+++ readme.md
@@ -0,0 +1,15 @@
+ <h1 style="text-align:center;">The magic Ball<h1>
+<img src="img/MagicBall8.png">
+<p style="font-size:18px;">It's a simple JS project that emulates a classic Magic Ball 8 game. <br><br> Where you answer with a simple answer , for example "No", "Yes", "Probably" is fun.<br><br>
+to play, you think of a question and click on the ball, in a moment it shows a message with your answer</p>
+
+## Goodluck 
+> to edit the responses you can go to js/main.js and change the response of the magicBall function
+
+<img src="img/answer.png">
+
+> In this part you can change the answers
+
+<img src="img/function.PNG">
+
+_Thanks for watching._
